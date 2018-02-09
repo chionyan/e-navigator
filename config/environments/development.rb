5f3cef9889ca2d5b14cb3da53156dce3518a7cfa
chionyan <ariariarissa@gmail.com>
2018-02-09 18:15:00 +0900

    環境設定、メーラークラス生成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,17 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => '587',
+    :domain => 'smtp.gmail.com',
+    :authentication => 'plain',
+    :user_name => 'ユーザー名@gmail.com',
+    :password => 'gmailパスワード'
+  }
 
   config.action_mailer.perform_caching = false
 
